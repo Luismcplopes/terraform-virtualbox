a6b5323f8999fc5737041fe722d149fdaa3299c8
Luis MCP Lopes <17655448+Luismcplopes@users.noreply.github.com>
2025-10-29 15:23:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,7 @@ resource "virtualbox_vm" "node" {
   image  = var.image
   cpus   = var.cpu_counts
   memory = var.memory
-  #user_data = file("user_data.sh")
-  #  terraform apply  -auto-approve
-
-
+ 
   network_adapter {
     type           = "bridged"
     #var.network_adapter_type
@@ -40,12 +37,18 @@ resource "null_resource" "ssh_docker" {
       "sudo sh test-docker.sh",
       "sudo usermod -aG docker vagrant",
       "echo install docker completed",
-      "sudo docker run -ti -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer-ce --privileged --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v ./portainer/data:/data  portainer/portainer",
+      "sudo docker run -d -ti --name stirling-pdf --restart unless-stopped -p 8888:8080 frooodle/s-pdf:latest",
+      "echo install stirling-pdf complete",
+      "sudo docker run  -d -ti --restart unless-stopped --name libre_speed -e PUID=1000 -e PGID=1000 -e TZ=Europe/London -v ./librespeed:/config -p 10101:80 lscr.io/linuxserver/librespeed:latest",
+      "echo install librespeed complete",
       "sudo docker run -ti -d --name dozzle -v /var/run/docker.sock:/var/run/docker.sock -p 8383:8080 amir20/dozzle:latest",
       "echo install dozzle complete",
+      "sudo docker run -d --name it-tools --restart unless-stopped -p 8080:80 ghcr.io/corentinth/it-tools:latest",
+      "echo install it-tools complete",
       "sudo mv 60-static-ip.yaml /etc/netplan/00-static-ip.yaml",
       "sudo chmod 600 /etc/netplan/00-static-ip.yaml",
       "sudo chmod 600 /etc/netplan/01-netcfg.yaml",
+      #"sudo rm -rf /etc/netplan/01-netcfg.yaml",
       "sudo netplan apply --config-file /etc/netplan/00-static-ip.yaml",
       "echo VM is completed"
     ]
@@ -58,11 +61,11 @@ resource "null_resource" "ssh_docker2" {
     user        = "vagrant"
     password = "vagrant"
     #private_key = file("/path/to/your/private/key.pem")
-    host        = "192.168.1.111"
+    host        = "10.0.55.90"
     }
   provisioner "remote-exec" {
     inline = [
-      "echo 'Hello, World'",
+      "echo Alterar para IP fixo",
       "sudo rm -rf /etc/netplan/01-netcfg.yaml",
       "sudo netplan apply --config-file /etc/netplan/00-static-ip.yaml",
       "echo VM is completed"
